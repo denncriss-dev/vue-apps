c626819cda7e0980d9cd567606b12bf28622efde
Edgar Hernandez <edgar.hernandezvh@gmail.com>
2022-08-03 03:34:37 +0000

    el h

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
-  import HelloWorld from './components/HelloWorld.vue'
-  import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import TheWelcome from './components/TheWelcome.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <p>modify</p>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+    <p>Hello</p>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
@@ -18,30 +24,30 @@
 </template>
 
 <style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
   header {
-    line-height: 1.5;
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
 
   .logo {
-    display: block;
-    margin: 0 auto 2rem;
+    margin: 0 2rem 0 0;
   }
 
-  @media (min-width: 1024px) {
-    header {
-      display: flex;
-      place-items: center;
-      padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-      margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-      display: flex;
-      place-items: flex-start;
-      flex-wrap: wrap;
-    }
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
   }
+}
 </style>
